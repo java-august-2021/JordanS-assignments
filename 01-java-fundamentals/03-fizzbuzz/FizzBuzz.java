2cbeb884e11c6da9d9ea4e926d74c3ff815fe167
Jordan Sullivan <jsulli725@gmail.com>
2021-08-06 13:51:26 -0400

    FizzBuzz

---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzz.java
@@ -0,0 +1,13 @@
+public class FizzBuzz {
+    public static String fizzBuzz(int num) {
+        if(num % 3 == 0 && num % 5 ==0){
+            return("FizzBuzz");
+        } else if(num % 5 == 0){
+            return("Buzz");
+        } else if(num % 3 == 0){
+            return("Fizz");
+        } else {
+            return Integer.toString(num);
+        }
+    }
+}
---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz myTest = new FizzBuzz();
+
+        System.out.println(myTest.fizzBuzz(3));
+        System.out.println(myTest.fizzBuzz(34));
+        System.out.println(myTest.fizzBuzz(10));
+        System.out.println(myTest.fizzBuzz(15));
+        System.out.println(myTest.fizzBuzz(16));
+        System.out.println(myTest.fizzBuzz(2));
+    
+    }
+}
